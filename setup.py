3ef959cd56368603cc0751957663fa6c226b7920
Aayush Mishra <aayushmm2162@gmail.com>
2025-06-10 11:11:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HealthMate',
+    version='0.0.0',
+    author='Aayush Mishra',
+    author_email='aayushm2162@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
